84b61a4b0306b49bfdcefbb9f3ceeb836bea59a4
koki-develop <kou.pg.0131@gmail.com>
2023-04-23 18:18:34 +0900

    ログ出力を追加

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const core = require("@actions/core");
 const tc = require("@actions/tool-cache");
 
 (async () => {
-  const url =
-    "https://github.com/koki-develop/qiita-cli/releases/download/v0.1.0/qiita_Linux_x86_64.tar.gz";
+  const version = (() => {
+    const version = "0.1.0"; // TODO: get from input
+    if (!version.startsWith("v")) {
+      return `v${version}`;
+    }
+  })();
+  core.info(`version: ${version}`);
+
+  const platform = process.platform;
+  const arch = process.arch;
+  core.info(`platform: ${platform}`);
+  core.info(`arch: ${arch}`);
+
+  core.info("Downloading...");
+  const url = `https://github.com/koki-develop/qiita-cli/releases/download/${version}/qiita_Linux_x86_64.tar.gz`;
   const cliPath = await tc.downloadTool(url);
+
+  core.info("Installing...");
   const extractedPath = await tc.extractTar(cliPath);
   const binPath = await tc.cacheDir(extractedPath, "qiita", "0.1.0");
   core.addPath(binPath);
+  core.info("Installed.");
 })().catch((err) => {
   core.setFailed(err.message);
 });
